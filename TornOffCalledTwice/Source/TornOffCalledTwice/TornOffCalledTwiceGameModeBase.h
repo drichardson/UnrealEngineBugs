c283b2f50cbf82a70993103d902b3903bb645b27
Doug Richardson <git@rekt.email>
2021-04-05 15:33:38 -0700

    wip

---
+++ TornOffCalledTwice/Source/TornOffCalledTwice/TornOffCalledTwiceGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TornOffCalledTwiceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TORNOFFCALLEDTWICE_API ATornOffCalledTwiceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
